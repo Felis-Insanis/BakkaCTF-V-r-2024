d4b00d2bed5a92a7972bbee0a41b570365df15d6
Felis-Insanis <132434409+Felis-Insanis@users.noreply.github.com>
2024-05-24 14:09:49 +0200

    utviklet nettside

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-alert("HTML ER SUPER")
+function superHTML() {
+    alert("HTML ER SUPER");
+};
+
+function changePic() {
+    let img = document.getElementById("clue").src="./f12.jpg";
+};
+
+alert("HTML ER SUPER");
